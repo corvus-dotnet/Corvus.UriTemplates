9f4de13635b6b77ed173d7a4a5c8e75eebd7ab80
Matthew Adams <matthew.adams@endjin.com>
2022-10-07 11:36:31 +0100

    Added a sandbox console app.

---
+++ Solutions/Sandbox/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json;
+using Corvus.UriTemplates;
+
+const string uriTemplate = "http://example.org/location{?value*}";
+
+using JsonDocument jsonValues = JsonDocument.Parse("{\"value\": { \"foo\": \"bar\", \"bar\": 3.4, \"baz\": \"bob\" }}");
+
+object? nullState = default;
+
+JsonUriTemplateResolver.TryResolveResult(uriTemplate.AsSpan(), false, jsonValues.RootElement, HandleResult, ref nullState);
+
+static void HandleResult(ReadOnlySpan<char> resolvedTemplate, ref object? state)
+{
+    Console.WriteLine(resolvedTemplate.ToString());
+}

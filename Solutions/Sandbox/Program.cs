9e37310c8b810180a33ed3f645253dba5736689e
Matthew Adams <matthew.adams@endjin.com>
2022-10-07 12:26:47 +0100

    Added some examples of using Json and Dictionary parameter providers.

--- Solutions/Sandbox/Program.cs
+++ Solutions/Sandbox/Program.cs
@@ -3,11 +3,14 @@ using Corvus.UriTemplates;
 
 const string uriTemplate = "http://example.org/location{?value*}";
 
-using JsonDocument jsonValues = JsonDocument.Parse("{\"value\": { \"foo\": \"bar\", \"bar\": 3.4, \"baz\": \"bob\" }}");
+using var jsonValues = JsonDocument.Parse("{\"value\": { \"foo\": \"bar\", \"bar\": 3.4, \"baz\": null }}");
+Dictionary<string, string> value = new() { { "foo", "bar" }, { "bar", "baz" }, { "baz", "bob" } };
+Dictionary<string, object?> parameters = new() { { "value", value } };
 
 object? nullState = default;
 
 JsonUriTemplateResolver.TryResolveResult(uriTemplate.AsSpan(), false, jsonValues.RootElement, HandleResult, ref nullState);
+DictionaryUriTemplateResolver.TryResolveResult(uriTemplate.AsSpan(), false, parameters, HandleResult, ref nullState);
 
 static void HandleResult(ReadOnlySpan<char> resolvedTemplate, ref object? state)
 {

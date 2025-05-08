77d4781e49c50e042c400a286c322acbf6ccf278
Ian Griffiths <Ian.Griffiths@endjin.com>
2025-05-08 11:45:05 +0100

    Exploded path parameter handling

--- Solutions/Sandbox/Program.cs
+++ Solutions/Sandbox/Program.cs
@@ -2,6 +2,13 @@
 using Corvus.UriTemplates.Benchmarking;
 
 
+IUriTemplateParser template2 = UriTemplateParserFactory.CreateParser("/weather{/rest*}");
+Console.WriteLine(template2.IsMatch("/weather"));
+Console.WriteLine(template2.IsMatch("/weather/"));
+Console.WriteLine(template2.IsMatch("/weather/one"));
+Console.WriteLine(template2.IsMatch("/weather/weather/or/not"));
+Console.WriteLine(template2.IsMatch("/weatherone")); // Doesn't match, because of the / in {/rest*}
+
 IUriTemplateParser template = UriTemplateParserFactory.CreateParser(
     "{scheme}://{host}/");
 template.IsMatch("http://example.com/");
